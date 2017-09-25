9b4ab6ecd39f50d40b6966bc459217a766cb3ee5
aditReza <lete.note3@gmail.com>
2017-09-25 20:01:59 +0700

    release 1

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -3,6 +3,7 @@
 class Animal {
     constructor(legs) {
         this._num_legs = 2
+        this.superpower = new Superpower()
     }
 }
 
@@ -38,6 +39,16 @@ class Chicken extends Animal {
     // constructor() {
     //     this._num_legs = legs
     // }
+
+}
+
+class Superpower {
+    use_laser_vision() {
+        return ' mengeluarkan laser dari matanya'
+    }
+    be_invisible() {
+        return ' akan menghilang'
+    }
 }
 
 let Animals = new Animal(4)
@@ -47,5 +58,8 @@ let Monyet = new Chimpanzee()
 let Rubah = new Fox()
 let Ayam = new Chicken()
 
-/*release 0 */ // parent assign legs =2 dan mengassign legs =4 untuk beberapa jenis hewan
+/* release 0 */ // parent assign legs =2 dan mengassign legs =4 untuk beberapa jenis hewan
 console.log(`Kodok berkaki ${Kodok._num_legs} Monyet berkaki ${Monyet._num_legs} Rubah juga berkaki${Rubah._num_legs}`)
+    /* release 1 */ // + class superpower, + method laser & invisible
+console.log(Rubah.superpower.use_laser_vision())
+console.log(Kodok.superpower.be_invisible())
