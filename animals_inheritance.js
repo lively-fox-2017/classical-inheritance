b89faa9737c6259e9b02bbf44ed83c688b8c8de6
jasoneffendi <jasoneffendi27@gmail.com>
2017-09-25 11:06:46 +0700

    Classical Inheritance Done

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,58 @@
 "use strict"
+class Animal {
+    constructor (name) {
+        this.legs = 4;
+        this.kelas = 'Mammalia';
+        this.name = name
+        this.superpower = new superpower(name)
+    }
+}
+
+
+class Frog extends Animal{
+    constructor () {
+        super('Frog')
+        // this.name = "Frog"
+    }
+}
+
+class Bat extends Animal{
+    constructor () {
+        super('Bat')
+        // this.name = "Bat"
+    }
+}
+
+class Fox extends Animal {
+    constructor () {
+        super('Fox')
+        // this.name = "Fox"
+    }
+}
+
+class superpower {
+    constructor(name) {
+        this.name = name
+    }
+
+    laser() {
+        console.log(`${this.name}` + ' Laser vision engaged')
+    }
+
+    invisible() {
+        console.log('Invisibility engaged')
+    }
+}
+let binatang = new Animal()
+console.log(binatang)
+
+let Kodok = new Frog()
+console.log(Kodok.legs)
+
+let Kelelawar = new Bat()
+console.log(Kelelawar.kelas)
+
+let Rubah = new Fox()
+console.log(Rubah.name + ' memiliki kaki sebanyak '+ Rubah.legs)
+
+Rubah.superpower.laser()
