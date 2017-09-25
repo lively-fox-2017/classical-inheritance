811aae443a7a9efdab3388817a0db9d2a789cc39
Saptanto Sindu <saptanto.sindu@gmail.com>
2017-09-25 14:20:25 +0700

    Commit Release 0

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,30 @@
 "use strict"
+class Animal {
+  constructor(name, birthday, foods) {
+    this.name = name;
+    this.birthday = new Date(birthday);
+    this.foods = foods;
+    this.eatable = true;
+  }
+}
+
+class Bat extends Animal {
+  constructor(name, birthday, foods) {
+    super(name, birthday, foods);
+    this.nocturnal = true;
+  }
+}
+
+class Monkey extends Animal {
+  constructor(name, birthday, foods, isTrained) {
+    super(name, birthday, foods);
+    this.eatable = false;
+    this.isTrained = isTrained;
+  }
+}
+
+let bat = new Bat('aselole', '2012-09-09', ['blood', 'fruits']);
+let monkey = new Monkey('joss', '2012-05-05', ['banana', 'fruits'], true);
+
+console.log(bat);
+console.log(monkey);
