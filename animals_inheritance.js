09d29b9640535bf1ecce1d8236335dfa9624fed4
Saptanto Sindu <saptanto.sindu@gmail.com>
2017-09-25 14:56:11 +0700

    Release 1 Super power animals

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -5,6 +5,15 @@ class Animal {
     this.birthday = new Date(birthday);
     this.foods = foods;
     this.eatable = true;
+    this._superpower = null;
+  }
+
+  setSuper() {
+    this._superpower = new SuperPower(this);
+  }
+
+  get superpower() {
+    return this._superpower;
   }
 }
 
@@ -23,8 +32,34 @@ class Monkey extends Animal {
   }
 }
 
+class SuperPower {
+  constructor(caller) {
+    this.caller = caller
+  }
+  use_laser_vision() {
+    return this.caller.name + " is using laser ================================> x (Bamm!)";
+  }
+
+  be_invisible() {
+    return this.caller.name + ' is Invisible (Sssss..)';
+  }
+}
+
 let bat = new Bat('aselole', '2012-09-09', ['blood', 'fruits']);
 let monkey = new Monkey('joss', '2012-05-05', ['banana', 'fruits'], true);
 
+console.log((bat instanceof(Animal)) ? "Bat Inherit Animal" : "False");
+console.log((monkey instanceof(Animal)) ? "Monkey Inherit Animal" : "False");
 console.log(bat);
 console.log(monkey);
+console.log('-'.repeat(30));
+
+// Become super power at night
+let time = Math.floor(Math.random() * 24);
+if ((time >= 18 && time <= 24) || (time >= 0 && time <= 6)) {
+  console.log('Time: ' + time);
+  bat.setSuper();
+  monkey.setSuper();
+  console.log(bat.superpower.be_invisible());
+  console.log(monkey.superpower.use_laser_vision());
+}
